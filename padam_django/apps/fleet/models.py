05c813f31992ada548346088f9343e528fcceb51
Théo Dorée <devtwoast@gmail.com>
2021-08-09 16:28:40 +0200

    fix: Typo on licence_place that should be licence_plate

--- padam_django/apps/fleet/models.py
+++ padam_django/apps/fleet/models.py
@@ -9,10 +9,10 @@ class Driver(models.Model):
 
 
 class Bus(models.Model):
-    licence_place = models.CharField("Name of the bus", max_length=10)
+    licence_plate = models.CharField("Name of the bus", max_length=10)
 
     class Meta:
         verbose_name_plural = "Buses"
 
     def __str__(self):
-        return f"Bus: {self.licence_place} (id: {self.pk})"
+        return f"Bus: {self.licence_plate} (id: {self.pk})"

b248f9671a91c68015b778feff172ff34608f2bc
Mad <damian.valadon@epitech.eu>
2021-10-28 11:01:23 +0200

    Modified admin/models in preparation for model constraints

--- padam_django/apps/fleet/models.py
+++ padam_django/apps/fleet/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from padam_django.apps.geography.models import Place
+
 
 class Driver(models.Model):
     user = models.OneToOneField('users.User', on_delete=models.CASCADE, related_name='driver')
@@ -16,3 +18,32 @@ class Bus(models.Model):
 
     def __str__(self):
         return f"Bus: {self.licence_place} (id: {self.pk})"
+
+
+class BusShift(models.Model):
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    bus_stops = models.ManyToManyField("BusStop")
+
+    departure = models.DateTimeField()
+    arrival = models.DateTimeField()
+
+    # Constraint Check if instance of shift has bus/driver and is not in timetable of departure/arrival of another
+    #   instance, UniqueConstraint doesnt stop from creating instances without the necessary constraints
+    class Meta:
+        # constraint = [
+        #     models.CheckConstraint(check=models.Q)
+        # ]
+        models.UniqueConstraint(fields=['bus', 'departure', 'arrival'], name='unique_bus_booking')
+        models.UniqueConstraint(fields=['driver', 'departure', 'arrival'], name='unique_driver_booking')
+
+    def __str__(self):
+        return f"Time Table: {self.departure} - {self.arrival} (id: {self.pk})"
+
+
+class BusStop(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+
+    # Need to figure out how to obtain name
+    # def __str__(self):
+    #     return f"Place: (id: {self.pk})"

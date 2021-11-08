a67a46743d96043ebaa1b9ba1bba3de1a9e78099
rcepre <renaudcepre@gmail.com>
2021-11-08 16:30:27 +0100

    Transform to M2M.

--- padam_django/apps/busshifts/admin.py
+++ padam_django/apps/busshifts/admin.py
@@ -1,15 +1,50 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.db.models import Q
+from django.forms import ModelForm
 
-# Register your models here.
 from padam_django.apps.busshifts.models import BusStop, BusShift
+from padam_django.apps.common.utils.time_utils import overlap
 
 
-class BusStopInline(admin.TabularInline):
-    model = BusStop
+class BusShiftForm(ModelForm):
+    class Meta:
+        model = BusShift
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(BusShiftForm, self).__init__(*args, **kwargs)
+        self.fields['stops'].queryset = BusStop.objects.order_by('time')
+
+    def clean(self):
+        stops = self.cleaned_data.get('stops')
+        bus = self.cleaned_data.get('bus')
+        driver = self.cleaned_data.get('driver')
+        if stops:
+            # Check the minimum number of bus stops.
+            if stops.count() < 2:
+                raise ValidationError('At least two stops are required.')
+
+            start = stops.order_by('time').first().time
+            end = stops.order_by('time').last().time
+
+            # Check if the bus is already used.
+            other_shifts = BusShift.objects.filter(Q(bus=bus) & ~Q(pk=self.instance.pk))
+            for s in other_shifts:
+                if overlap((s.get_start_time(), s.get_end_time()), (start, end)):
+                    raise ValidationError(f"The bus {bus} is already used for these dates")
+
+            # Check if the driver is already drive a bus at this date.
+            other_shifts = BusShift.objects.filter(Q(driver=driver) & ~Q(pk=self.instance.pk))
+            for s in other_shifts:
+                if overlap((s.get_start_time(), s.get_end_time()), (start, end)):
+                    raise ValidationError(f"The driver {driver} is already driving at theses dates")
+
+        return self.cleaned_data
 
 
 class BusShiftAdmin(admin.ModelAdmin):
-    inlines = [BusStopInline]
+    form = BusShiftForm
 
 
 admin.site.register(BusShift, BusShiftAdmin)
--- padam_django/apps/busshifts/models.py
+++ padam_django/apps/busshifts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 from padam_django.apps.fleet.models import Bus, Driver
 from padam_django.apps.geography.models import Place
 
@@ -8,17 +7,16 @@ from padam_django.apps.geography.models import Place
 class BusShift(models.Model):
     driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
     bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
-
+    stops = models.ManyToManyField("BusStop")
 
     def get_start_time(self):
-        return self.busstop_set.all()[0].time
+        return self.stops.order_by('time')[0].time
 
     def get_end_time(self):
-        return self.busstop_set.reverse()[0].time
+        return self.stops.order_by('-time')[0].time
 
     def get_duration(self):
         return self.get_end_time() - self.get_start_time()
-
 
     def __str__(self):
         return f"{self.get_duration()}"
@@ -27,10 +25,6 @@ class BusShift(models.Model):
 class BusStop(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
     time = models.DateTimeField()
-    shift = models.ForeignKey(BusShift, on_delete=models.CASCADE)
-
-    class Meta:
-        ordering = ['time']
 
     def __str__(self):
-        return f"Stop at {self.place}"
+        return f"Stop at {self.place}, {self.time}"

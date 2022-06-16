23d45c891a2a27f6e5be6d7259341cd3e9784a89
riscaldo <riccardo85ferrara@gmail.com>
2022-06-16 18:45:49 +0700

    sort stops on arrival time, add filters

--- padam_django/apps/rides/admin.py
+++ padam_django/apps/rides/admin.py
@@ -4,8 +4,16 @@ from .models import BusStop, BusShift
 class BusStopInline(admin.TabularInline):
     model = BusStop
     extra = 10
+    ordering = ('time', )
 
 class BusShiftAdmin(admin.ModelAdmin):
     inlines = [BusStopInline]
+    list_display = ('driver', 'bus')
+
+class BusStopAdmin(admin.ModelAdmin):
+    list_display = ('place', 'time', 'busshift')
+    list_filter = ['busshift', 'time']
 
 admin.site.register(BusShift, BusShiftAdmin)
+
+admin.site.register(BusStop, BusStopAdmin)

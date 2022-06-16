0b7998cca840d87f469e14d88f58b254ff92a493
riscaldo <riccardo85ferrara@gmail.com>
2022-06-16 12:38:07 +0700

    bus i-stop is only positive

--- padam_django/apps/rides/models.py
+++ padam_django/apps/rides/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class BusStop(models.Model):
     bus = models.ForeignKey('fleet.Bus', null=True, on_delete=models.CASCADE)
     place = models.ForeignKey('geography.Place', null=True, on_delete=models.CASCADE)
-    stop = models.IntegerField()
+    stop = models.PositiveIntegerField()
 
     def __str__(self):
         return f"Stop: {self.stop}-{self.place.name} Bus: {self.bus.licence_plate} (id: {self.pk})"

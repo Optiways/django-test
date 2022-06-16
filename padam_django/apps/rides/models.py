5161463e7924c69984a626190693f91e4ca4b253
riscaldo <riccardo85ferrara@gmail.com>
2022-06-16 12:34:08 +0700

    create BusStop model

--- padam_django/apps/rides/models.py
+++ padam_django/apps/rides/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class BusStop(models.Model):
+    bus = models.ForeignKey('fleet.Bus', null=True, on_delete=models.CASCADE)
+    place = models.ForeignKey('geography.Place', null=True, on_delete=models.CASCADE)
+    stop = models.IntegerField()
+
+    def __str__(self):
+        return f"Stop: {self.stop}-{self.place.name} Bus: {self.bus.licence_plate} (id: {self.pk})"

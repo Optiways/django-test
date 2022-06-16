b9882c7fee0f06b01aa9bcec10b427e618c75ab0
riscaldo <riccardo85ferrara@gmail.com>
2022-06-16 12:51:37 +0700

    add arrival time

--- padam_django/apps/rides/models.py
+++ padam_django/apps/rides/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+import datetime
 
 
 class BusStop(models.Model):
     bus = models.ForeignKey('fleet.Bus', null=True, on_delete=models.CASCADE)
     place = models.ForeignKey('geography.Place', null=True, on_delete=models.CASCADE)
+    time = models.DateTimeField('arrival time', null=True)
     stop = models.PositiveIntegerField()
 
     def __str__(self):

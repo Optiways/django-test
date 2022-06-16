4e94d6bb51e22b6dd7738cb9f517f42e291d7a14
riscaldo <riccardo85ferrara@gmail.com>
2022-06-16 13:50:16 +0700

    BusStop has manywone relation with BusShift

--- padam_django/apps/rides/models.py
+++ padam_django/apps/rides/models.py
@@ -2,15 +2,18 @@ from django.db import models
 import datetime
 
 
+class BusShift(models.Model):
+    bus = models.ForeignKey('fleet.Bus', null=True, on_delete=models.CASCADE)
+    driver = models.ForeignKey('fleet.Driver', null=True, on_delete=models.CASCADE)
+    # busstop = models.ForeignKey(BusStop, null=True, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"Shift: {self.bus.licence_plate} - {self.driver.user.username} (id: {self.pk})"
+
 class BusStop(models.Model):
     place = models.ForeignKey('geography.Place', null=True, on_delete=models.CASCADE)
     time = models.DateTimeField('arrival time', null=True)
     stop = models.PositiveIntegerField()
+    busshift = models.ForeignKey(BusShift, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return f"Stop: {self.stop}-{self.place.name} (id: {self.pk})"
-
-
-class BusShift(models.Model):
-    bus = models.ForeignKey('fleet.Bus', null=True, on_delete=models.CASCADE)
-    driver = models.ForeignKey('fleet.Driver', null=True, on_delete=models.CASCADE)
+        return f"Stop: {self.stop} - {self.place.name} (id: {self.pk})"

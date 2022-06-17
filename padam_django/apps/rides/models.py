6c38e54f27ea63b4acf4a530230665457710798d
riscaldo <riccardo85ferrara@gmail.com>
2022-06-17 15:01:59 +0700

    save ride_arr_time and ride_dep_time in db

--- padam_django/apps/rides/models.py
+++ padam_django/apps/rides/models.py
@@ -8,29 +8,26 @@ class BusShift(models.Model):
     ride_arr_time = models.DateTimeField('ride arrival time', null=True)
     ride_dep_time = models.DateTimeField('ride departure time', null=True)
     
-
     def arr_time(self):
         b = BusShift.objects.get(id=self.pk)
         arr_time = b.busstop_set.aggregate(Max('time'))['time__max']
-        # it would be better to stock the info in the db instead of the above 
-        # function every time
-        # b.ride_arr_time = b.busstop_set.aggregate(Max('time'))['time__max']
-        # b.save()
+        # it would be better to stock the info in the db only once instead of 
+        # do it every time (maybe using signal?)
+        b.ride_arr_time = b.busstop_set.aggregate(Max('time'))['time__max']
+        b.save()
         return arr_time
 
     def dep_time(self):
         b = BusShift.objects.get(id=self.pk)
         dep_time = b.busstop_set.aggregate(Min('time'))['time__min']
-        # it would be better to stock the info in the db instead of the above 
-        # function every time
-        # b.ride_dep_time = b.busstop_set.aggregate(Min('time'))['time__min']
-        # b.save()
+        # it would be better to stock the info in the db only once instead of 
+        # do it every time (maybe using signal?)
+        b.ride_dep_time = b.busstop_set.aggregate(Min('time'))['time__min']
+        b.save()
         return dep_time
     
-
-
     def __str__(self):
-        return f"Shift: {self.bus.licence_plate} - {self.driver.user.username} (id: {self.pk})"
+        return f"Shift: { self.ride_dep_time.strftime('%d/%m/%y %H:%M') } - { self.ride_arr_time.strftime('%d/%m/%y %H:%M') } (id: {self.pk})"
 
 class BusStop(models.Model):
     place = models.ForeignKey('geography.Place', null=True, on_delete=models.CASCADE)
@@ -39,3 +36,4 @@ class BusStop(models.Model):
 
     def __str__(self):
         return f"Stop: {self.time} - {self.place.name} (id: {self.pk})"
+

78984c6b24f72d7d04c1f2ad28900be010bb64b2
riscaldo <riccardo85ferrara@gmail.com>
2022-06-17 14:21:22 +0700

    add rides arrival and departure time

--- padam_django/apps/rides/admin.py
+++ padam_django/apps/rides/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import BusStop, BusShift
 
+
 class BusStopInline(admin.TabularInline):
     model = BusStop
     extra = 10
@@ -9,6 +10,11 @@ class BusStopInline(admin.TabularInline):
 class BusShiftAdmin(admin.ModelAdmin):
     inlines = [BusStopInline]
     list_display = ('driver', 'bus')
+    readonly_fields = ['arr_time', 'dep_time']
+    fields = ['driver', 'bus', 'dep_time', 'arr_time']
+    # fields = ['driver', 'bus', 'ride_dep_time', 'ride_arr_time']
+    # readonly_fields = ['ride_arr_time', 'ride_dep_time']
+
 
 class BusStopAdmin(admin.ModelAdmin):
     list_display = ('place', 'time', 'busshift')
--- padam_django/apps/rides/models.py
+++ padam_django/apps/rides/models.py
@@ -1,9 +1,33 @@
 from django.db import models
+from django.db.models import Max, Min
 
 
 class BusShift(models.Model):
     bus = models.ForeignKey('fleet.Bus', null=True, on_delete=models.CASCADE)
     driver = models.ForeignKey('fleet.Driver', null=True, on_delete=models.CASCADE)
+    ride_arr_time = models.DateTimeField('ride arrival time', null=True)
+    ride_dep_time = models.DateTimeField('ride departure time', null=True)
+    
+
+    def arr_time(self):
+        b = BusShift.objects.get(id=self.pk)
+        arr_time = b.busstop_set.aggregate(Max('time'))['time__max']
+        # it would be better to stock the info in the db instead of the above 
+        # function every time
+        # b.ride_arr_time = b.busstop_set.aggregate(Max('time'))['time__max']
+        # b.save()
+        return arr_time
+
+    def dep_time(self):
+        b = BusShift.objects.get(id=self.pk)
+        dep_time = b.busstop_set.aggregate(Min('time'))['time__min']
+        # it would be better to stock the info in the db instead of the above 
+        # function every time
+        # b.ride_dep_time = b.busstop_set.aggregate(Min('time'))['time__min']
+        # b.save()
+        return dep_time
+    
+
 
     def __str__(self):
         return f"Shift: {self.bus.licence_plate} - {self.driver.user.username} (id: {self.pk})"

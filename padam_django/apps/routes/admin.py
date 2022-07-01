219b737eddb276caa3cb48cb2e5a56e985aa17cd
Teddy Tinjic <teddy.tinjic@epitech.eu>
2022-07-01 19:14:10 +0200

    feat(BusShift/BusStop): register admin part

--- padam_django/apps/routes/admin.py
+++ padam_django/apps/routes/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+from . import forms
+
+
+@admin.register(models.BusStop)
+class BusStopAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk', 'place', 'schedule_time'
+    )
+
+
+@admin.register(models.BusShift)
+class BusShiftAdmin(admin.ModelAdmin):
+    form = forms.BusShiftForm
+    list_display = (
+        'pk', 'bus', 'driver', 'departure_time', 'arrival_time', 'total_time'
+    )
+    fields = (
+       'bus', 'driver', 'stops', 'departure_time', 'arrival_time', 'total_time'
+    )
+    readonly_fields = ('departure_time', 'arrival_time', 'total_time')

ce9971b86cf256f9189217b9a0ff5c749f5dd58c
Saifeddine <saifeddine.bejaoui@gmail.com>
2022-09-07 22:09:00 +0200

    Add and register ModelAdmin: BusStopAdmin, BusShiftForm

--- padam_django/apps/fleet/admin.py
+++ padam_django/apps/fleet/admin.py
@@ -1,13 +1,26 @@
+
+# Django import.
 from django.contrib import admin
 
+# App import
 from . import models
+from .forms import BusShiftForm
 
+
+
+@admin.register(models.BusStop)
+class BusStopAdmin(admin.ModelAdmin):
+    list_display = ('uid', 'driver', 'bus', 'arrival',)
+
+@admin.register(models.BusShift)
+class BusShiftAdmin(admin.ModelAdmin):
+    form = BusShiftForm
+    list_display = ('uid', 'driver', 'bus', 'departure', 'arrival',)
+    readonly_fields = ( 'travel_time', )
 
 @admin.register(models.Bus)
 class BusAdmin(admin.ModelAdmin):
-    pass
-
-
+    pass 
 @admin.register(models.Driver)
 class DriverAdmin(admin.ModelAdmin):
     pass

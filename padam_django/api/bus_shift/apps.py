f9512db4f0718ef2403f86c7206594f099fcd396
Charlotte Gosselin <gosselin.charlotte@hotmail.fr>
2024-04-15 12:35:29 +0200

    clean files

--- padam_django/api/bus_shift/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BusShiftConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bus_shift'
--- padam_django/api/stop/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StopConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stop'

21475f05a2650c5d73a7cc07a22ebcd18a6bfd99
WBinquet <william.binquet@laposte.net>
2024-10-27 19:24:35 +0100

    Made admin less minimal

--- padam_django/apps/travel/admin.py
+++ padam_django/apps/travel/admin.py
@@ -1,13 +1,58 @@
+from django import forms
 from django.contrib import admin
 
 from . import models
 
 
+class BusStopAdminMixin:
+    list_display = ("place", "ts_requested", "has_boarded")
+    readonly_fields = (
+        "ts_estimated",
+        "ts_boarded",
+        "has_boarded",
+    )
+
+
+class StartBusStopForm(forms.ModelForm):
+    class Meta:
+        model = models.StartBusStop
+        fields = (
+            "user",
+            "place",
+            "ts_requested",
+            "ts_estimated",
+            "ts_boarded",
+            "has_boarded",
+        )
+
+
 @admin.register(models.StartBusStop)
-class StartBusStopAdmin(admin.ModelAdmin):
-    pass
+class StartBusStopAdmin(BusStopAdminMixin, admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "user",
+    ) + BusStopAdminMixin.list_display
+
+    form = StartBusStopForm
+
+
+class EndBusStopForm(forms.ModelForm):
+    class Meta:
+        model = models.EndBusStop
+        fields = (
+            "start",
+            "place",
+            "ts_requested",
+            "ts_estimated",
+            "ts_boarded",
+            "has_boarded",
+        )
 
 
 @admin.register(models.EndBusStop)
-class EndBusStopAdmin(admin.ModelAdmin):
-    pass
+class EndBusStopAdmin(BusStopAdminMixin, admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "start",
+    ) + BusStopAdminMixin.list_display
+    form = EndBusStopForm
